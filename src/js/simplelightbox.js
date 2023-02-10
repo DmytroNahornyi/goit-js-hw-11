c3e7a5f871692e8ea7c4ea42813b4d9712fee8b1
Dmytro Nahornyi <d3049999@gmail.com>
2023-02-10 15:54:17 +0200

    update 11 homework

--- src/js/simplelightbox.js
+++ src/js/simplelightbox.js
@@ -1,19 +1,15 @@
-export const fetchImages = async (inputValue, pageNr) => {
-    return await fetch(
-        `https://pixabay.com/api/?key=33445005-670ff6718a077cc10d3f17f6a&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-    )
-        .then(async response => {
-            if (!response.ok) {
-                if (response.status === 404) {
-                    return [];
-                }
-                throw new Error(response.status);
-            }
-            return await response.json();
-        })
-        .catch(error => {
-            console.error(error);
-        });
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+axios.defaults.params = {
+  key: '33445005-670ff6718a077cc10d3f17f6a&q',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 40,
+  safesearch: true,
 };
 
-// https://pixabay.com/api/docs/
+export const fetchImages = async (inputValue, page) => {
+  const request = await axios.get(`/?q=${inputValue}&page=${page}`);
+  return request.data;
+};
